291b1d7b1dc542bbb108914424842ea20c33819e
JiYinchuan <jiyinchuan@haichuang.pro>
2021-07-15 19:34:46 +0800

    修复结构问题

--- hc-sdk-pay-ali/src/main/java/pro/haichuang/framework/sdk/alipay/util/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 支付宝支付核心工具包
- */
-@NonNullApi
-@NonNullFields
-package pro.haichuang.framework.sdk.alipay.util;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
